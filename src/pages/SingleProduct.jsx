3a211f4d1deea05422df49aa59a0afeffc4131b3
lucariggi92 <riggiluca92@gmail.com>
2025-12-10 19:44:08 +0100

    feat: single product card

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,10 +1,31 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
+import axios from "axios"
+import { useEffect, useState } from "react";
 
-export default function SingleProduct(){
-    const {id} =useParams();
-    return(
+export default function SingleProduct() {
+    const { id } = useParams();
+    const [product, setProduct] = useState(null);
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        setLoading(true)
+
+        axios.get(`https://fakestoreapi.com/products/${id}`)
+            .then((resp) => {
+                setProduct(resp.data);
+            }).catch((err) => {
+                console.log(err);
+                alert("Il prodotto non esiste");
+                navigate("/prodotti");
+            }).finally(() => {
+                setLoading(false);
+
+            })
+    }, [id]);
+    return (
         <section>
-           <h1>pagina singolo prodotto {id}</h1> 
+            <h1>{product.title}</h1>
         </section>
     )
 
